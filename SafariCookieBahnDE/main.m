90ff65a364521de3a3637d37379db0b6e593cf3f
Karsten Silkenbäumer <ksi@kluks.de>
2013-01-04 22:48:46 +0100

    Find and modify expiration date for bahn.de settings cookies

--- SafariCookieBahnDE/main.m
+++ SafariCookieBahnDE/main.m
@@ -3,7 +3,7 @@
 //  SafariCookieBahnDE
 //
 //  Created by Karsten Silkenbäumer on 04.01.13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2013 kluks.de. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -13,10 +13,23 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+        for (NSHTTPCookie *c in [cookieStorage cookies])
+        {
+            if ( [[c domain] isEqualToString:@".bahn.de"] ) {
+                if ( [[c name] hasPrefix:@"DB4-pb"] ) {
+                    NSDate *date = [[c expiresDate] dateByAddingTimeInterval:60*60*24*360];
+                    
+                    NSDictionary *prop = [c properties];
+                    [prop setValue:date forKey:@"Expires"];
+                    
+                    NSHTTPCookie *newCookie = [NSHTTPCookie cookieWithProperties:prop];
+                    
+                    [cookieStorage setCookie:newCookie];
+                }
+            }
+        }
         
     }
     return 0;
 }
-
