caad470529710542fc8fefd5a810b4d1e36657d7
Karsten Silkenbäumer <ksi@kluks.de>
2013-01-04 22:13:31 +0100

    Initial Commit

---
+++ SafariCookieBahnDE/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SafariCookieBahnDE
+//
+//  Created by Karsten Silkenbäumer on 04.01.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
